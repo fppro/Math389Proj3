840de4a6073dc19fc85509bf23cb53de6254f6bb
bvandersloot <benvds@umich.edu>
2015-04-10 15:30:09 -0400

    added secant method

--- newton.py
+++ newton.py
@@ -19,10 +19,33 @@ def newtonsMethod(f, x, startPoint):
     while abs(fEval(curX) - 0) > 0.001:
         print curX, fEval(curX)
         slope = fprimeEval(curX)
+        if fprimeEval(curX) == 0:
+            return curX
         curX = curX - (fEval(curX) / fprimeEval(curX))
 
     return curX
 
+
+def secantMethod(f, x, startPoint1, startPoint2):
+    ''' Take a function f of a variable (symbol) x. Run Secant Method
+    starting at startPoint '''
+    
+    x1 = startPoint1
+    x2 = startPoint2
+    
+    # The lambdified version allow us to directly compute values
+    fEval = lambdify(x, f, 'numpy')
+
+    # Run until we get very close to a root
+    while abs(fEval(x2) - 0) > 0.001:
+        print x1, x2, fEval(x2)
+        slope = (fEval(x2)-fEval(x1))/(x2-x1)
+        if slope == 0:
+            return x2
+        x1 = x2
+        x2 = x2 - (fEval(x2) / slope)
+
+    return x2
 
 def main():
     # This makes the "variable" for sympy
@@ -31,10 +54,11 @@ def main():
     # The function we want to analyze
     y = x**3 - x**2 - x + 1.1
     
-    root = newtonsMethod(y, x, 20.00)
-
+    root = newtonsMethod(y, x, 2.00)
     print "Final answer: ", root
 
+    #root = secantMethod(y, x, 2.00, 3.00)
+    #print "Final answer: ", root
 
 if __name__ == '__main__':
     main()
