bf069ddc8fbbae16161a21ac766369bd0222f908
bvandersloot <benvds@umich.edu>
2015-04-10 15:40:17 -0400

    added halleys method

--- newton.py
+++ newton.py
@@ -25,10 +25,37 @@ def newtonsMethod(f, x, startPoint):
 
     return curX
 
+def halleysMethod(f, x, startPoint):
+    ''' Take a function f of a variable (symbol) x. Run Halley's Method
+    starting at startPoint '''
+    
+    curX = startPoint
+    
+    # Sympy does this for us
+    fprime = f.diff(x)
+
+    # Sympy does this for us
+    fdoublePrime = fprime.diff(x)
+
+    # The lambdified version allow us to directly compute values
+    fEval = lambdify(x, f, 'numpy')
+    fprimeEval = lambdify(x, fprime, 'numpy')
+    fdoublePrimeEval = lambdify(x, fdoublePrime, 'numpy')
+
+    # Run until we get very close to a root
+    while abs(fEval(curX) - 0) > 0.001:
+        print curX, fEval(curX)
+        numer = 2*fEval(curX)*fprimeEval(curX)
+        denom = 2*fprimeEval(curX)*fprimeEval(curX) - fEval(curX)*fdoublePrimeEval(curX) 
+        if denom == 0:
+            return curX
+        curX = curX - (numer / denom)
+
+    return curX
 
 def secantMethod(f, x, startPoint1, startPoint2):
     ''' Take a function f of a variable (symbol) x. Run Secant Method
-    starting at startPoint '''
+    starting at startPoints '''
     
     x1 = startPoint1
     x2 = startPoint2
@@ -54,7 +81,7 @@ def main():
     # The function we want to analyze
     y = x**3 - x**2 - x + 1.1
     
-    root = newtonsMethod(y, x, 2.00)
+    root = halleysMethod(y, x, 2.00)
     print "Final answer: ", root
 
     #root = secantMethod(y, x, 2.00, 3.00)
