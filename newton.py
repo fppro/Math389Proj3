fd5a8e8c4fbe81481b3b66832ee558a35e2f328a
bvandersloot <benvds@umich.edu>
2015-04-11 11:22:18 -0400

    all methods now shifted over, example in main

--- newton.py
+++ newton.py
@@ -24,7 +24,6 @@ def newtonsMethod(f, x, startPoint, epsilon):
         history.append(abs(fEval(curX)))
         if len(history) > 1 and history[-1] > history[-2]:
             history = []
-        print history
         slope = fprimeEval(curX)
         if fprimeEval(curX) == 0:
             return []
@@ -33,7 +32,7 @@ def newtonsMethod(f, x, startPoint, epsilon):
     history.append(abs(fEval(curX)))
     return history
 
-def halleysMethod(f, x, startPoint):
+def halleysMethod(f, x, startPoint, epsilon):
     ''' Take a function f of a variable (symbol) x. Run Halley's Method
     starting at startPoint '''
     
@@ -49,19 +48,23 @@ def halleysMethod(f, x, startPoint):
     fEval = lambdify(x, f, 'numpy')
     fprimeEval = lambdify(x, fprime, 'numpy')
     fdoublePrimeEval = lambdify(x, fdoublePrime, 'numpy')
+    history = []
 
     # Run until we get very close to a root
-    while abs(fEval(curX) - 0) > 0.001:
-        print curX, fEval(curX)
+    while abs(fEval(curX) - 0) > epsilon:
+        history.append(abs(fEval(curX)))
+        if len(history) > 1 and history[-1] > history[-2]:
+            history = []
         numer = 2*fEval(curX)*fprimeEval(curX)
         denom = 2*fprimeEval(curX)*fprimeEval(curX) - fEval(curX)*fdoublePrimeEval(curX) 
         if denom == 0:
-            return curX
+            return []
         curX = curX - (numer / denom)
 
-    return curX
+    history.append(abs(fEval(curX)))
+    return history
 
-def secantMethod(f, x, startPoint1, startPoint2):
+def secantMethod(f, x, startPoint1, startPoint2, epsilon):
     ''' Take a function f of a variable (symbol) x. Run Secant Method
     starting at startPoints '''
     
@@ -71,16 +74,26 @@ def secantMethod(f, x, startPoint1, startPoint2):
     # The lambdified version allow us to directly compute values
     fEval = lambdify(x, f, 'numpy')
 
+    history = []
+
     # Run until we get very close to a root
-    while abs(fEval(x2) - 0) > 0.001:
-        print x1, x2, fEval(x2)
+    while abs(fEval(x2) - 0) > epsilon:
+        history.append(abs(fEval(x2)))
+        if len(history) > 1 and history[-1] > history[-2]:
+            history = []
         slope = (fEval(x2)-fEval(x1))/(x2-x1)
         if slope == 0:
             return x2
         x1 = x2
         x2 = x2 - (fEval(x2) / slope)
 
-    return x2
+    history.append(abs(fEval(x2)))
+    return history
+
+def convergenceRate(path):
+    np.polyfit(range(len(path)),  np.log10(path), 1)
+    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(range(len(path)), np.log10(path))
+    return [slope, r_value**2]
 
 def main():
     # This makes the "variable" for sympy
@@ -89,12 +102,9 @@ def main():
     # The function we want to analyze
     y = x**3 - x**2 - x + 1.1
     
-    path = newtonsMethod(y, x, 2.00, .00000001)
+    path = secantMethod(y, x, 2.00, 2.10, .00000001)
     print "Final answer: ", path
-    print "Logs :", np.log10(path)
-    print np.polyfit(range(len(path)), np.log10(path), 1)
-    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(range(len(path)), np.log10(path))
-    print slope, r_value**2
+    print convergenceRate(path)
 
     #root = secantMethod(y, x, 2.00, 3.00)
     #print "Final answer: ", root
